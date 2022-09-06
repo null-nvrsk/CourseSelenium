b614245470da1673e47057fd8b4f8f178794f036
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-09-06 21:33:58 +0300

    Add 3 tests - hidden modal, alerts, frames

--- src/test/java/FirstClassTest.java
+++ src/test/java/FirstClassTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -67,6 +68,88 @@ public class FirstClassTest {
 
 //        WebElement signupLink = driver.findElement(By.xpath("//a[contains(@href,'r.php')]"));
 //        signupLink.click();
+
+        driver.quit();
+    }
+
+    @Test
+    public void testHiddenModalDialog(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1800, 900));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.navigate().to("https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/hidden");
+        driver.switchTo().frame("frame_examples");
+
+        WebElement buttonOk = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='okButton']")));
+        buttonOk.click();
+
+        WebElement textWelcome = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='awesome']/h1")));
+        Assert.assertEquals("Thanks!", textWelcome.getText());
+
+        driver.quit();
+    }
+
+    @Test
+    public void testJavascriptInfoAlert(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://javascript.info/alert-prompt-confirm");
+
+        // alert
+        WebElement runButton = driver.findElement(By.xpath("(//a[@data-action='run'])[1]"));
+        runButton.click();
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        runButton.click();
+        System.out.println("Text from alert = " + alert.getText());
+        alert.accept();
+
+        // confirm
+        WebElement confirmButton = driver.findElement(By.xpath("(//a[@data-action='run'])[5]"));
+        confirmButton.click();
+
+        Alert confirm = driver.switchTo().alert();
+        confirm.dismiss();
+        confirm.accept();
+
+        // prompt
+        WebElement promptButton = driver.findElement(By.xpath("(//a[@data-action='run'])[2]"));
+        promptButton.click();
+
+        Alert prompt = driver.switchTo().alert();
+        prompt.sendKeys("123");
+        confirm.accept();
+        confirm.accept();
+
+        driver.quit();
+    }
+
+    @Test
+    public void testIframe(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // <label for="SeleniumCourse">number of lecture</label>
+        // <input type="number" id="seleniumId" min="10" max="100">
+        driver.get("https://jsbin.com/bigubugosu/edit?html,output");
+
+        WebElement mainFrame = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@class='stretch']")));
+        driver.switchTo().frame(mainFrame);
+
+        WebElement childFrame = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@name='JS Bin Output ']")));
+        driver.switchTo().frame(childFrame);
+
+        WebElement inputField = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='seleniumId']")));
+        inputField.sendKeys("66");
 
         driver.quit();
     }
