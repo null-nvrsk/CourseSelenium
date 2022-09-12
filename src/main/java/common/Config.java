12b13c7d6e6f092450e9d56bc4572994c6577089
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-09-12 15:41:34 +0300

    Create multi-driver builder

---
+++ src/main/java/common/Config.java
@@ -0,0 +1,12 @@
+package common;
+
+public class Config {
+
+    /**
+     * Specify the browser and platform for tests
+     * CHROME_WINDOWS
+     * CHROME_LINUX
+     * MOZILLA_WINDOWS
+     **/
+    public static final String BROWSER_AND_PLATFORM = "CHROME_WINDOWS";
+}
