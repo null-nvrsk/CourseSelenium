4448de25874f51c5f1a1dcbc0bba7878c107e7b5
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-09-13 22:30:54 +0300

    Update BasePage

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -1,10 +1,32 @@
 package pages.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static constants.Constant.TimeoutVariables.EXPLICIT_WAIT;
 
 public class BasePage {
     WebDriver driver;
     public BasePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    /**
+     * Navigation to specific URL
+     */
+    public void goToUrl(String url) {
+        driver.get(url);
+    }
+
+    /**
+     * Wait for visibility element in DOM model
+     */
+    public WebElement waitElementIsVisible(WebElement element){
+        new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT)).until(ExpectedConditions.visibilityOf(element));
+        return element;
     }
 }
