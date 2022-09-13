f58452bb0a865dc89b86fbc945cf0ff58ef940c1
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-09-13 16:42:59 +0300

    Init driver

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -9,4 +9,12 @@ public class Config {
      * MOZILLA_WINDOWS
      **/
     public static final String BROWSER_AND_PLATFORM = "CHROME_WINDOWS";
+    /**
+     * Clear browser cookies after each iteration
+     */
+    public static final Boolean CLEAR_COOKIES = true;
+    /**
+     * To keep the browser open after all tests
+     */
+    public static final Boolean HOLD_BROWSER_OPEN = false;
 }
