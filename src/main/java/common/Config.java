2b94be06f2298434130d2a6b57d522c15122ad52
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-09-22 12:17:58 +0300

    Add clear reports function

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -9,12 +9,13 @@ public class Config {
      * MOZILLA_WINDOWS
      **/
     public static final String BROWSER_AND_PLATFORM = "CHROME_WINDOWS";
-    /**
-     * Clear browser cookies after each iteration
-     */
+
+    /** Clear browser cookies after each iteration */
     public static final Boolean CLEAR_COOKIES = false;
-    /**
-     * To keep the browser open after all tests
-     */
+
+    /** To keep the browser open after all tests */
     public static final Boolean HOLD_BROWSER_OPEN = false;
+
+    /** Clear folders with screenshots before starting build */
+    public static final Boolean CLEAR_REPORTS_FOLDER = true;
 }
