e9e706c58833018b5c3039ff8fbf04013cb92ff8
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-09-22 14:00:46 +0300

    Add listener for screenshots

--- src/test/java/tests/base/Listener.java
+++ src/test/java/tests/base/Listener.java
@@ -1,5 +1,6 @@
 package tests.base;
 
+import io.qameta.allure.Attachment;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
@@ -32,6 +33,11 @@ public class Listener implements TestWatcher {
             throw new RuntimeException(e);
         }
 
+        attachScreenshotToAllure(ts);
+    }
 
+    @Attachment
+    public byte[] attachScreenshotToAllure(TakesScreenshot takesScreenshot){
+        return takesScreenshot.getScreenshotAs(OutputType.BYTES);
     }
 }
