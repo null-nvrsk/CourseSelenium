2232f112f2d3df7ad645e3d0b0db8e9c1ca5b2a3
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-10-29 18:43:16 +0300

    Migrate to Selenide

--- src/main/java/pages/telecomunications/MobileTopUpPage.java
+++ src/main/java/pages/telecomunications/MobileTopUpPage.java
@@ -1,78 +1,78 @@
-package pages.telecomunications;
-
-import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import pages.base.BasePage;
-
-public class MobileTopUpPage extends BasePage {
-
-    public MobileTopUpPage(WebDriver driver) {
-        super(driver);
-    }
-
-    private final By buttonWallet = By.xpath("//div[contains(text(), 'My wallet')]");
-    private final By inputPhoneNumber = By.xpath("//input[@data-qa-node='phone-number']");
-    private final By inputAmount = By.xpath("//input[@data-qa-node='amount']");
-    private final By inputCardNumber = By.xpath("//input[@data-qa-node='numberdebitSource']");
-    private final By inputCardExpDate = By.xpath("//input[@data-qa-node='expiredebitSource']");
-    private final By inputCardCvv = By.xpath("//input[@data-qa-node='cvvdebitSource']");
-    private final By buttonAddToCart = By.xpath("//button[@data-qa-node='submit']");
-    private final By textPaymentDetails = By.xpath("//div[@data-qa-node='details']");
-
-
-    /**
-     * Choose a card from the wallet
-     * @return Mobile Top-Up Page Object
-     */
-     public MobileTopUpPage selectCardFromWallet(){
-         driver.findElement(buttonWallet).click();
-         return this;
-     }
-
-    /**
-     * Enter phone number excluding country code
-     * @param number phone number
-     * @return Mobile Top-Up Page Object
-     */
-     public MobileTopUpPage enterPhoneNumber(String number){
-         driver.findElement(inputPhoneNumber).sendKeys(number);
-         return this;
-     }
-
-     public MobileTopUpPage enterAmount(String amount){
-         driver.findElement(inputAmount).sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-         driver.findElement(inputAmount).sendKeys(amount);
-         return this;
-     }
-
-    public MobileTopUpPage enterCardNumber(String cardNumber){
-        driver.findElement(inputCardNumber).sendKeys(cardNumber);
-        return this;
-    }
-
-    public MobileTopUpPage enterCardExpDate(String expDate){
-        driver.findElement(inputCardExpDate).sendKeys(expDate);
-        return this;
-    }
-
-    public MobileTopUpPage enterCardCvv(String cvv){
-        driver.findElement(inputCardCvv).sendKeys(cvv);
-        return this;
-    }
-
-    public MobileTopUpPage submitCart(){
-        driver.findElement(buttonAddToCart).click();
-        return this;
-    }
-
-    public MobileTopUpPage checkPaymentDetailsIsPresentInTheCart(String expectedText){
-        WebElement details = driver.findElement(textPaymentDetails);
-        waitElementIsVisible(details);
-
-        Assertions.assertEquals(expectedText, details.getText());
-        return this;
-    }
-}
+//package pages.telecomunications;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.Keys;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import pages.base.BasePage;
+//
+//public class MobileTopUpPage extends BasePage {
+//
+//    public MobileTopUpPage(WebDriver driver) {
+//        super(driver);
+//    }
+//
+//    private final By buttonWallet = By.xpath("//div[contains(text(), 'My wallet')]");
+//    private final By inputPhoneNumber = By.xpath("//input[@data-qa-node='phone-number']");
+//    private final By inputAmount = By.xpath("//input[@data-qa-node='amount']");
+//    private final By inputCardNumber = By.xpath("//input[@data-qa-node='numberdebitSource']");
+//    private final By inputCardExpDate = By.xpath("//input[@data-qa-node='expiredebitSource']");
+//    private final By inputCardCvv = By.xpath("//input[@data-qa-node='cvvdebitSource']");
+//    private final By buttonAddToCart = By.xpath("//button[@data-qa-node='submit']");
+//    private final By textPaymentDetails = By.xpath("//div[@data-qa-node='details']");
+//
+//
+//    /**
+//     * Choose a card from the wallet
+//     * @return Mobile Top-Up Page Object
+//     */
+//     public MobileTopUpPage selectCardFromWallet(){
+//         driver.findElement(buttonWallet).click();
+//         return this;
+//     }
+//
+//    /**
+//     * Enter phone number excluding country code
+//     * @param number phone number
+//     * @return Mobile Top-Up Page Object
+//     */
+//     public MobileTopUpPage enterPhoneNumber(String number){
+//         driver.findElement(inputPhoneNumber).sendKeys(number);
+//         return this;
+//     }
+//
+//     public MobileTopUpPage enterAmount(String amount){
+//         driver.findElement(inputAmount).sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
+//         driver.findElement(inputAmount).sendKeys(amount);
+//         return this;
+//     }
+//
+//    public MobileTopUpPage enterCardNumber(String cardNumber){
+//        driver.findElement(inputCardNumber).sendKeys(cardNumber);
+//        return this;
+//    }
+//
+//    public MobileTopUpPage enterCardExpDate(String expDate){
+//        driver.findElement(inputCardExpDate).sendKeys(expDate);
+//        return this;
+//    }
+//
+//    public MobileTopUpPage enterCardCvv(String cvv){
+//        driver.findElement(inputCardCvv).sendKeys(cvv);
+//        return this;
+//    }
+//
+//    public MobileTopUpPage submitCart(){
+//        driver.findElement(buttonAddToCart).click();
+//        return this;
+//    }
+//
+//    public MobileTopUpPage checkPaymentDetailsIsPresentInTheCart(String expectedText){
+//        WebElement details = driver.findElement(textPaymentDetails);
+//        waitElementIsVisible(details);
+//
+//        Assertions.assertEquals(expectedText, details.getText());
+//        return this;
+//    }
+//}
--- src/test/java/tests/telecomunications/positive/MobileTopUpPositiveTest.java
+++ src/test/java/tests/telecomunications/positive/MobileTopUpPositiveTest.java
@@ -1,31 +1,31 @@
-package tests.telecomunications.positive;
-
-import constants.Constant;
-import org.junit.jupiter.api.Test;
-import tests.base.BaseTest;
-
-import static constants.Constant.MobileRefillTestData.*;
-import static constants.Constant.Urls.MOBILE_PAYMENT_URL;
-
-public class MobileTopUpPositiveTest extends BaseTest {
-
-    @Test
-    public void checkIsRedirectToAuth(){
-        basePage.goToUrl(MOBILE_PAYMENT_URL);
-        mobileTopUpPage.selectCardFromWallet();
-        basePage.isAuthWidgetPresent();
-    }
-
-    @Test
-    public void checkMinimumRefillAmount(){
-        basePage.goToUrl(MOBILE_PAYMENT_URL);
-        mobileTopUpPage.enterPhoneNumber(MOBILE_PAYMENT_PHONE_NUMBER)
-                .enterCardNumber(MOBILE_PAYMENT_CARD)
-                .enterCardExpDate(MOBILE_PAYMENT_CARD_EXP_DATE)
-                .enterCardCvv(MOBILE_PAYMENT_CARD_CVV)
-                .enterAmount("1")
-                .submitCart()
-                .checkPaymentDetailsIsPresentInTheCart("Mobile payment. Phone number +380993004801");
-
-    }
-}
+//package tests.telecomunications.positive;
+//
+//import constants.Constant;
+//import org.junit.jupiter.api.Test;
+//import tests.base.BaseTest;
+//
+//import static constants.Constant.MobileRefillTestData.*;
+//import static constants.Constant.Urls.MOBILE_PAYMENT_URL;
+//
+//public class MobileTopUpPositiveTest extends BaseTest {
+//
+//    @Test
+//    public void checkIsRedirectToAuth(){
+//        basePage.goToUrl(MOBILE_PAYMENT_URL);
+//        mobileTopUpPage.selectCardFromWallet();
+//        basePage.isAuthWidgetPresent();
+//    }
+//
+//    @Test
+//    public void checkMinimumRefillAmount(){
+//        basePage.goToUrl(MOBILE_PAYMENT_URL);
+//        mobileTopUpPage.enterPhoneNumber(MOBILE_PAYMENT_PHONE_NUMBER)
+//                .enterCardNumber(MOBILE_PAYMENT_CARD)
+//                .enterCardExpDate(MOBILE_PAYMENT_CARD_EXP_DATE)
+//                .enterCardCvv(MOBILE_PAYMENT_CARD_CVV)
+//                .enterAmount("1")
+//                .submitCart()
+//                .checkPaymentDetailsIsPresentInTheCart("Mobile payment. Phone number +380993004801");
+//
+//    }
+//}
